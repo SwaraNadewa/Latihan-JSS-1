661881601a8f2fa1c80dd53d3820968fa0beaaaa
SwaraNadewa <snain45@yahoo.com>
2024-09-23 23:24:35 +0700

    Coba1

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function replaceName() {
+
+    let name = prompt("Siapa Nama Anda ?", "")
+    document.getElementById("name"), innerHTML = name
+}
+
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceName();
+
+})
